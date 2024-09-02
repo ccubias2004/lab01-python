ddfac35a18e9101d36b8518b8b5d1b10ca901545
ccubias2004 <123924518+ccubias2004@users.noreply.github.com>
2024-09-02 23:58:41 +0000

    lab01 finished

---
+++ hello.py
@@ -0,0 +1,7 @@
+# Name: Carlos Cubias
+# Date: 8/29/24
+# File Purpose: Multiple hello functions
+def helloworld():
+    return "Hello World!"
+def helloname(name):
+    return "Hello " + name + "!"
---
+++ main.py
@@ -0,0 +1,7 @@
+# Name: Carlos Cubias
+# Date: 8/29/24
+# File Purpose: Lab01 main program driver program
+from hello import *
+print ("*** TUFFY TITAN HELLO PROGRAM ***")
+print(helloworld())
+print(helloname("Steve"))
